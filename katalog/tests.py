226f2e75a9ad20c80d9b0b231fe522ddef28f5c9
fahiradh <fahiraadindiah16@gmail.com>
2022-09-15 10:15:54 +0700

    update test cases

--- katalog/tests.py
+++ katalog/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
+from katalog.models import CatalogItem  
+from katalog.views import show_katalog
 
-# Create your tests here.
+class CatalogTestCase(TestCase):
+    def setUp(self):
+        CatalogItem.objects.create(
+            item_name="Gucci Horsebit 1955", 
+            item_price=36000000, 
+            item_stock=3,
+            description="Original",
+            rating=4,
+            item_url="https://www.gucci.com/us/en/st/capsule/gucci-1955-horsebit-handbag")
+
+    def test_models(self):
+        gucci = CatalogItem.objects.get(item_name="Gucci Horsebit 1955")
+        self.assertEquals(gucci.item_name, "Gucci Horsebit 1955")
+
+class TestUrls(TestCase):
+    def test_urls(self):
+        url = reverse("katalog:show_katalog")
+        self.assertEquals(resolve(url).func, show_katalog)
+
